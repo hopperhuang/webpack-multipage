b299d844240de6ee9554653b75de57507315ec09
黄家炜 <huangjiawei@innotechx.com>
2018-03-18 15:03:14 +0800

    1.fix: fix bug when mulit js script are placed in the same folder will cause error

--- getFile.js
+++ getFile.js
@@ -4,7 +4,7 @@ const Glob = require('glob')
 module.exports = function getFiles () {
   let filenames
   const reg = /^\.\/src\/pages\/(.+)\/index.js$/
-  let files = Glob.sync('./src/pages/**/*.js')
+  let files = Glob.sync('./src/pages/**/index.js')
   const _filenames = files.map(file => reg.exec(file))
   filenames = _filenames.map(file => [file[1], file[0]])
   return filenames
